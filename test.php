d6266c5fb8ffd128c36b82cda9768136a8cf8410
qdinar <qdinar@gmail.com>
2016-01-03 20:35:35 +0300

    new tests to fix

--- test.php
+++ test.php
@@ -5,49 +5,51 @@ require('../url_to_absolute.php');
 require('phpuri.php');
 
 $tests=array(
-  array('rel' => 'g:h', 'result' => 'g:h'),
-  array('rel' => 'g',   'result' => 'http://a/b/c/g'),
-  array('rel' => './g', 'result' => 'http://a/b/c/g'),
-  array('rel' => 'g/',  'result' => 'http://a/b/c/g/'),
-  array('rel' => '/g',  'result' => 'http://a/g'),
-  array('rel' => '//g', 'result' => 'http://g'),
-  array('rel' => 'g?y', 'result' => 'http://a/b/c/g?y'),
-  array('rel' => '#s', 'result' => 'http://a/b/c/d;p?q#s'),
-  array('rel' => 'g#s', 'result' => 'http://a/b/c/g#s'),
-  array('rel' => 'g?y#s', 'result' => 'http://a/b/c/g?y#s'),
-  array('rel' => ';x', 'result' => 'http://a/b/c/;x'),
-  array('rel' => 'g;x', 'result' => 'http://a/b/c/g;x'),
-  array('rel' => 'g;x?y#s', 'result' => 'http://a/b/c/g;x?y#s'),
-  array('rel' => '.', 'result' => 'http://a/b/c/'),
-  array('rel' => './', 'result' => 'http://a/b/c/'),
-  array('rel' => '..', 'result' => 'http://a/b/'),
-  array('rel' => '../', 'result' => 'http://a/b/'),
-  array('rel' => '../g', 'result' => 'http://a/b/g'),
-  array('rel' => '../..', 'result' => 'http://a/'),
-  array('rel' => '../../', 'result' => 'http://a/'),
-  array('rel' =>'../../g','result' =>'http://a/g'),
-  array('rel' =>'g.','result' =>'http://a/b/c/g.'),
-  array('rel' =>'.g','result' =>'http://a/b/c/.g'),
-  array('rel' =>'g..','result' =>'http://a/b/c/g..'),
-  array('rel' =>'..g','result' =>'http://a/b/c/..g'),
-  array('rel' =>'./../g','result' =>'http://a/b/g'),
-  array('rel' =>'./g/.','result' =>'http://a/b/c/g/'),
-  array('rel' =>'g/./h','result' =>'http://a/b/c/g/h'),
-  array('rel' =>'g/../h','result' =>'http://a/b/c/h'),
-  array('rel' =>'g;x=1/./y','result' =>'http://a/b/c/g;x=1/y'),
-  array('rel' =>'g;x=1/../y','result' =>'http://a/b/c/y'),
-  array('rel' =>'g?y/./x','result' =>'http://a/b/c/g?y/./x'),
-  array('rel' =>'g?y/../x','result' =>'http://a/b/c/g?y/../x'),
-  array('rel' =>'g#s/./x','result' =>'http://a/b/c/g#s/./x'),
-  array('rel' =>'g#s/../x','result' =>'http://a/b/c/g#s/../x')
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g:h', 'result' => 'g:h'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g',   'result' => 'http://a/b/c/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => './g', 'result' => 'http://a/b/c/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g/',  'result' => 'http://a/b/c/g/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '/g',  'result' => 'http://a/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '//g', 'result' => 'http://g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g?y', 'result' => 'http://a/b/c/g?y'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '#s', 'result' => 'http://a/b/c/d;p?q#s'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g#s', 'result' => 'http://a/b/c/g#s'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g?y#s', 'result' => 'http://a/b/c/g?y#s'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => ';x', 'result' => 'http://a/b/c/;x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g;x', 'result' => 'http://a/b/c/g;x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'g;x?y#s', 'result' => 'http://a/b/c/g;x?y#s'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '.', 'result' => 'http://a/b/c/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => './', 'result' => 'http://a/b/c/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '..', 'result' => 'http://a/b/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '../', 'result' => 'http://a/b/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '../g', 'result' => 'http://a/b/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '../..', 'result' => 'http://a/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => '../../', 'result' => 'http://a/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'../../g','result' =>'http://a/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g.','result' =>'http://a/b/c/g.'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'.g','result' =>'http://a/b/c/.g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g..','result' =>'http://a/b/c/g..'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'..g','result' =>'http://a/b/c/..g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'./../g','result' =>'http://a/b/g'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'./g/.','result' =>'http://a/b/c/g/'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g/./h','result' =>'http://a/b/c/g/h'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g/../h','result' =>'http://a/b/c/h'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g;x=1/./y','result' =>'http://a/b/c/g;x=1/y'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g;x=1/../y','result' =>'http://a/b/c/y'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g?y/./x','result' =>'http://a/b/c/g?y/./x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g?y/../x','result' =>'http://a/b/c/g?y/../x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g#s/./x','result' =>'http://a/b/c/g#s/./x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' =>'g#s/../x','result' =>'http://a/b/c/g#s/../x'),
+  array( 'base' => 'http://a/b/c/d;p?q', 'rel' => 'ө/',  'result' => 'http://a/b/c/ө/'),
+  array( 'base' => 'http://a/b/c/.', 'rel' => 'g/',  'result' => 'http://a/b/c/g/'),
+  array( 'base' => 'http://a/b/c/..', 'rel' => 'g/',  'result' => 'http://a/b/g/'),
 );
 
 # rel2abs
 $start = microtime();
-$base = 'http://a/b/c/d;p?q';
 list($successes, $failures) = array(0,0);
 foreach($tests as $test){
-  if(($r = rel2abs($test['rel'], $base)) == $test['result']){
+  if(($r = rel2abs($test['rel'], $test['base'])) == $test['result']){
     $successes++;
   } else {
     $failures++;
@@ -59,10 +61,9 @@ echo "rel2abs:         successes -> $successes, failures => $failures, elapsed t
 
 # url_to_absolute
 $start = microtime();
-$base = 'http://a/b/c/d;p?q';
 list($successes, $failures) = array(0,0);
 foreach($tests as $test){
-  if(($r = url_to_absolute($base, $test['rel'])) == $test['result']){
+  if(($r = url_to_absolute($test['base'], $test['rel'])) == $test['result']){
     $successes++;
   } else {
     $failures++;
@@ -74,9 +75,9 @@ echo "url_to_absolute: successes -> $successes, failures => $failures, elapsed t
 
 # phpuri
 $start = microtime();
-$base = phpUri::parse('http://a/b/c/d;p?q');
 list($successes, $failures) = array(0,0);
 foreach($tests as $test){
+  $base = phpUri::parse($test['base']);
   if(($r = $base->join($test['rel'])) == $test['result']){
     $successes++;
   } else {
